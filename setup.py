b99b55b79c131a66221368478434a9a393509f07
Andreas Lindh <andreas.lindh@loopstone.com>
2021-03-06 14:47:25 +0100

    version='1.0.8'

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(name='ipxact2systemverilog',
       python_requires='>=3',
-      version='1.0.7',
+      version='1.0.8',
       description='Generate VHDL, SystemVerilog, html, rst, md, pdf from an IPXACT description',
       long_description=readme(),
       classifiers=[
