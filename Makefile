oddball/ipxact2systemverilog

commit bb2d251722acf3be583fc0b263cb9e1155fa39c4

WIP: Modernize Python project
* Create a pyproject.toml
  - setup.py and MANIFEST.in are not longer needed (TBC)
* Updated the Topics with
  +  Software Development :: Code Generators
  +  Software Development :: Embedded Systems
* Added instuctions how to build locally to README.md

Signed-off-by: Jan Vermaete <jan.vermaete@gmail.com>

pyproject.toml
@@ -1,0 +1,39 @@
+[project]
+description = "Generate VHDL, SystemVerilog, html, rst, md, pdf, c headers from an IPXACT description"
+readme = "README.md"
+version = "1.0.23"
+name = "ipxact2systemverilog"
+authors = [
+  {name = "oddball"},
+]
+keywords = ["ipxact2systemverilog", "ipxact2vhdl", "VHDL", "SystemVerilog", "html", "rst", "md", "pdf", "IPXACT"]
+requires-python = ">= 3"
+dependencies = [
+  "docutils",
+  "lxml",
+  "mdutils",
+  "restructuredtext-lint",
+  "sphinx",
+  "sphinxcontrib-wavedrom",
+  "rstcloth",
+]
+classifiers = [
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Developers",
+  "Topic :: Software Development :: Code Generators",
+  "Topic :: Software Development :: Embedded Systems",
+  "Programming Language :: Python :: 3",
+]
+
+[build-system]
+requires = ["setuptools"]
+build-backend = "setuptools.build_meta"
+
+[project.urls]
+Repository = "https://github.com/oddball/ipxact2systemverilog"
+
+[tool.setuptools]
+include-package-data = true
+
+[tool.setuptools.package-data]
+ipxact2systemverilog = ["xml/*.xsd"]

MANIFEST.in
@@ -1,3 +1,0 @@
-include README.md
-include ipxact2systemverilog/xml/*.xsd
-include requirements.txt

setup.py
@@ -1,32 +1,0 @@
-from setuptools import setup
-
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(name='ipxact2systemverilog',
-      python_requires='>=3',
-      version='1.0.23',
-      description='Generate VHDL, SystemVerilog, html, rst, md, pdf, c headers from an IPXACT description',
-      long_description_content_type="text/markdown",
-      long_description=readme(),
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Programming Language :: Python :: 3',
-        'Topic :: Text Processing :: Linguistic',
-      ],
-      keywords='ipxact2systemverilog ipxact2vhdl VHDL SystemVerilog html rst md pdf IPXACT',
-      url='https://github.com/oddball/ipxact2systemverilog',
-      author='oddball',
-      license='GPL',
-      packages=['ipxact2systemverilog'],
-      install_requires=requirements,
-      scripts=['bin/ipxact2rst', 'bin/ipxact2md', 'bin/ipxact2systemverilog', 'bin/ipxact2vhdl', 'bin/ipxact2c'],
-      include_package_data=True,
-      zip_safe=False)

commit 58e2a92f3e5fd033d7a347db08bc9e61817cd4a2

Correct mistake of using bin files in ipxact2systemverilog package Mistake probably introduced in commit bb2d251 Add cli.py for generating the packages. Include scripts in pyproject.toml file to use functions of cli_py
This work was sponsored by OIP Sensor Systems

Signed-off-by: Thomas Vreys <Thomas.vreys999@gmail.com>

.gitignore
@@ -37,3 +37,5 @@ obj_dir/
 
 example.exe.dSYM/
 example.exe
+
+ipxact2systemverilog/ipxact2systemverilog.egg-info/

Makefile
@@ -18,43 +18,43 @@ all: gen
 
 gen:
         # no config
-	bin/ipxact2systemverilog --srcFile example/input/test.xml --destDir example/output
-	bin/ipxact2rst --srcFile example/input/test.xml --destDir example/output
-	bin/ipxact2md --srcFile example/input/test.xml --destDir example/output
-	bin/ipxact2vhdl --srcFile example/input/test.xml --destDir example/output
-	bin/ipxact2md --srcFile example/input/test.xml --destDir example/output
-	bin/ipxact2c --srcFile example/input/test.xml --destDir example/output
+	ipxact2systemverilog --srcFile example/input/test.xml --destDir example/output
+	ipxact2rst --srcFile example/input/test.xml --destDir example/output
+	ipxact2md --srcFile example/input/test.xml --destDir example/output
+	ipxact2vhdl --srcFile example/input/test.xml --destDir example/output
+	ipxact2md --srcFile example/input/test.xml --destDir example/output
+	ipxact2c --srcFile example/input/test.xml --destDir example/output
 	pandoc -s example/output/example.rst -o example/output/example.html
 	pandoc -s example/output/example.rst -o example/output/example.rtf
 	pandoc -s example/output/example.rst -o example/output/example.docx
 
         # default config
-	bin/ipxact2systemverilog --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
-	bin/ipxact2rst --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
-	bin/ipxact2md --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
-	bin/ipxact2vhdl --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
-	bin/ipxact2c --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
+	ipxact2systemverilog --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
+	ipxact2rst --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
+	ipxact2md --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
+	ipxact2vhdl --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
+	ipxact2c --srcFile example/input/test.xml --destDir example/output_default  --config example/input/default.ini
 
         # no default config
-	bin/ipxact2systemverilog --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
-	bin/ipxact2rst --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
-	bin/ipxact2md --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
-	bin/ipxact2vhdl --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
-	bin/ipxact2c --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
+	ipxact2systemverilog --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
+	ipxact2rst --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
+	ipxact2md --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
+	ipxact2vhdl --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
+	ipxact2c --srcFile example/input/test.xml --destDir example/output_no_default  --config example/input/no_default.ini
 
         # RestructuredText and Sphinx with Wavedrom
-	bin/ipxact2rst --srcFile example/input/test.xml --destDir example/output_sphinx  --config example/input/sphinx.ini
+	ipxact2rst --srcFile example/input/test.xml --destDir example/output_sphinx  --config example/input/sphinx.ini
 	sphinx-build example/output_sphinx example/output_sphinx/build -q -b latex
 	make -C example/output_sphinx/build
 	cp example/output_sphinx/build/example.pdf example/output_sphinx
 
         #  test
-	bin/ipxact2systemverilog --srcFile example/input/test2.xml --destDir example/output
-	bin/ipxact2rst --srcFile example/input/test2.xml --destDir example/output
-	bin/ipxact2md --srcFile example/input/test2.xml --destDir example/output
-	bin/ipxact2vhdl --srcFile example/input/test2.xml --destDir example/output
-	bin/ipxact2md --srcFile example/input/test2.xml --destDir example/output
-	bin/ipxact2c --srcFile example/input/test2.xml --destDir example/output
+	ipxact2systemverilog --srcFile example/input/test2.xml --destDir example/output
+	ipxact2rst --srcFile example/input/test2.xml --destDir example/output
+	ipxact2md --srcFile example/input/test2.xml --destDir example/output
+	ipxact2vhdl --srcFile example/input/test2.xml --destDir example/output
+	ipxact2md --srcFile example/input/test2.xml --destDir example/output
+	ipxact2c --srcFile example/input/test2.xml --destDir example/output
 
 
 compile: 

bin/ipxact2c
@@ -1,36 +1,0 @@
-#!/usr/bin/env python3
-
-import configparser
-import sys
-import argparse
-
-from ipxact2systemverilog.ipxact2hdlCommon import ipxactParser
-from ipxact2systemverilog.ipxact2hdlCommon import ipxact2otherGenerator
-from ipxact2systemverilog.ipxact2hdlCommon import cAddressBlock
-from ipxact2systemverilog.ipxact2hdlCommon import DEFAULT_INI
-from ipxact2systemverilog.validate import validate
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ipxact2c')
-    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
-    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
-    parser.add_argument('-c', '--config', help="configuration ini file")
-
-    args, unknown_args = parser.parse_known_args()
-
-    if not validate(args.srcFile):
-        print(f"{args.srcFile} doesn't validate")
-        sys.exit(1)
-
-    config = configparser.ConfigParser()
-    if args.config:
-        config.read_dict(DEFAULT_INI)
-        config.read(args.config)
-    else:
-        config.read_dict(DEFAULT_INI)
-
-    e = ipxactParser(args.srcFile, config)
-    document = e.returnDocument()
-    generator = ipxact2otherGenerator(args.destDir, config)
-    generator.generate(cAddressBlock, document)

bin/ipxact2md
@@ -1,34 +1,0 @@
-#!/usr/bin/env python3
-import configparser
-import sys
-import argparse
-
-from ipxact2systemverilog.ipxact2hdlCommon import ipxactParser
-from ipxact2systemverilog.ipxact2hdlCommon import ipxact2otherGenerator
-from ipxact2systemverilog.ipxact2hdlCommon import mdAddressBlock
-from ipxact2systemverilog.ipxact2hdlCommon import DEFAULT_INI
-from ipxact2systemverilog.validate import validate
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ipxact2md')
-    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
-    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
-    parser.add_argument('-c', '--config', help="configuration ini file")
-
-    args, unknown_args = parser.parse_known_args()
-
-    if not validate(args.srcFile):
-        print(f"{args.srcFile} doesn't validate")
-        sys.exit(1)
-
-    config = configparser.ConfigParser()
-    if args.config:
-        config.read_dict(DEFAULT_INI)
-        config.read(args.config)
-    else:
-        config.read_dict(DEFAULT_INI)
-
-    e = ipxactParser(args.srcFile, config)
-    document = e.returnDocument()
-    generator = ipxact2otherGenerator(args.destDir, config)
-    generator.generate(mdAddressBlock, document)

bin/ipxact2rst
@@ -1,35 +1,0 @@
-#!/usr/bin/env python3
-
-import configparser
-import sys
-import argparse
-
-from ipxact2systemverilog.ipxact2hdlCommon import ipxactParser
-from ipxact2systemverilog.ipxact2hdlCommon import ipxact2otherGenerator
-from ipxact2systemverilog.ipxact2hdlCommon import rstAddressBlock
-from ipxact2systemverilog.ipxact2hdlCommon import DEFAULT_INI
-from ipxact2systemverilog.validate import validate
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ipxact2rst')
-    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
-    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
-    parser.add_argument('-c', '--config', help="configuration ini file")
-
-    args, unknown_args = parser.parse_known_args()
-
-    if not validate(args.srcFile):
-        print(f"{args.srcFile} doesn't validate")
-        sys.exit(1)
-
-    config = configparser.ConfigParser()
-    if args.config:
-        config.read_dict(DEFAULT_INI)
-        config.read(args.config)
-    else:
-        config.read_dict(DEFAULT_INI)
-
-    e = ipxactParser(args.srcFile, config)
-    document = e.returnDocument()
-    generator = ipxact2otherGenerator(args.destDir, config)
-    generator.generate(rstAddressBlock, document)

bin/ipxact2systemverilog
@@ -1,36 +1,0 @@
-#!/usr/bin/env python3
-
-import configparser
-import sys
-import argparse
-
-from ipxact2systemverilog.ipxact2hdlCommon import ipxactParser
-from ipxact2systemverilog.ipxact2hdlCommon import ipxact2otherGenerator
-from ipxact2systemverilog.ipxact2hdlCommon import systemVerilogAddressBlock
-from ipxact2systemverilog.ipxact2hdlCommon import DEFAULT_INI
-from ipxact2systemverilog.validate import validate
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ipxact2systemverilog')
-    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
-    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
-    parser.add_argument('-c', '--config', help="configuration ini file")
-
-    args, unknown_args = parser.parse_known_args()
-
-    if not validate(args.srcFile):
-        print(f"{args.srcFile} doesn't validate")
-        sys.exit(1)
-
-    config = configparser.ConfigParser()
-    if args.config:
-        config.read_dict(DEFAULT_INI)
-        config.read(args.config)
-    else: 
-        config.read_dict(DEFAULT_INI)
-
-    e = ipxactParser(args.srcFile, config)
-    document = e.returnDocument()
-    generator = ipxact2otherGenerator(args.destDir, config)
-    generator.generate(systemVerilogAddressBlock, document)

bin/ipxact2vhdl
@@ -1,36 +1,0 @@
-#!/usr/bin/env python3
-
-import configparser
-import sys
-import argparse
-
-from ipxact2systemverilog.ipxact2hdlCommon import ipxactParser
-from ipxact2systemverilog.ipxact2hdlCommon import ipxact2otherGenerator
-from ipxact2systemverilog.ipxact2hdlCommon import vhdlAddressBlock
-from ipxact2systemverilog.ipxact2hdlCommon import DEFAULT_INI
-from ipxact2systemverilog.validate import validate
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ipxact2vhdl')
-    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
-    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
-    parser.add_argument('-c', '--config', help="configuration ini file")
-
-    args, unknown_args = parser.parse_known_args()
-
-    if not validate(args.srcFile):
-        print(f"{args.srcFile} doesn't validate")
-        sys.exit(1)
-
-    config = configparser.ConfigParser()
-    if args.config:
-        config.read_dict(DEFAULT_INI)
-        config.read(args.config)
-    else:
-        config.read_dict(DEFAULT_INI)
-
-    e = ipxactParser(args.srcFile, config)
-    document = e.returnDocument()
-    generator = ipxact2otherGenerator(args.destDir, config)
-    generator.generate(vhdlAddressBlock, document)

ipxact2systemverilog/cli.py
@@ -1,0 +1,134 @@
+import configparser
+import sys
+import argparse
+
+from .ipxact2hdlCommon import ipxactParser
+from .ipxact2hdlCommon import ipxact2otherGenerator
+from .ipxact2hdlCommon import cAddressBlock
+from .ipxact2hdlCommon import mdAddressBlock
+from .ipxact2hdlCommon import rstAddressBlock
+from .ipxact2hdlCommon import systemVerilogAddressBlock
+from .ipxact2hdlCommon import vhdlAddressBlock
+from .ipxact2hdlCommon import DEFAULT_INI
+from .validate import validate
+
+def main_c():
+    parser = argparse.ArgumentParser(description='ipxact2c')
+    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
+    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
+    parser.add_argument('-c', '--config', help="configuration ini file")
+
+    args, unknown_args = parser.parse_known_args()
+
+    if not validate(args.srcFile):
+        print(f"{args.srcFile} doesn't validate")
+        sys.exit(1)
+
+    config = configparser.ConfigParser()
+    if args.config:
+        config.read_dict(DEFAULT_INI)
+        config.read(args.config)
+    else:
+        config.read_dict(DEFAULT_INI)
+
+    e = ipxactParser(args.srcFile, config)
+    document = e.returnDocument()
+    generator = ipxact2otherGenerator(args.destDir, config)
+    generator.generate(cAddressBlock, document)
+
+def main_md():
+    parser = argparse.ArgumentParser(description='ipxact2md')
+    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
+    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
+    parser.add_argument('-c', '--config', help="configuration ini file")
+
+    args, _ = parser.parse_known_args()
+
+    if not validate(args.srcFile):
+        print(f"{args.srcFile} doesn't validate")
+        sys.exit(1)
+
+    config = configparser.ConfigParser()
+    if args.config:
+        config.read_dict(DEFAULT_INI)
+        config.read(args.config)
+    else:
+        config.read_dict(DEFAULT_INI)
+
+    e = ipxactParser(args.srcFile, config)
+    document = e.returnDocument()
+    generator = ipxact2otherGenerator(args.destDir, config)
+    generator.generate(mdAddressBlock, document)
+
+def main_rst():
+    parser = argparse.ArgumentParser(description='ipxact2rst')
+    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
+    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
+    parser.add_argument('-c', '--config', help="configuration ini file")
+
+    args, _ = parser.parse_known_args()
+
+    if not validate(args.srcFile):
+        print(f"{args.srcFile} doesn't validate")
+        sys.exit(1)
+
+    config = configparser.ConfigParser()
+    if args.config:
+        config.read_dict(DEFAULT_INI)
+        config.read(args.config)
+    else:
+        config.read_dict(DEFAULT_INI)
+
+    e = ipxactParser(args.srcFile, config)
+    document = e.returnDocument()
+    generator = ipxact2otherGenerator(args.destDir, config)
+    generator.generate(rstAddressBlock, document)
+
+def main_systemverilog():
+    parser = argparse.ArgumentParser(description='ipxact2systemverilog')
+    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
+    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
+    parser.add_argument('-c', '--config', help="configuration ini file")
+
+    args, _ = parser.parse_known_args()
+
+    if not validate(args.srcFile):
+        print(f"{args.srcFile} doesn't validate")
+        sys.exit(1)
+
+    config = configparser.ConfigParser()
+    if args.config:
+        config.read_dict(DEFAULT_INI)
+        config.read(args.config)
+    else: 
+        config.read_dict(DEFAULT_INI)
+
+    e = ipxactParser(args.srcFile, config)
+    document = e.returnDocument()
+    generator = ipxact2otherGenerator(args.destDir, config)
+    generator.generate(systemVerilogAddressBlock, document)
+
+def main_vhdl():
+    parser = argparse.ArgumentParser(description='ipxact2vhdl')
+    parser.add_argument('-s', '--srcFile', help='ipxact xml input file', required=True)
+    parser.add_argument('-d', '--destDir', help="write generated file to dir", required=True)
+    parser.add_argument('-c', '--config', help="configuration ini file")
+
+    args, _ = parser.parse_known_args()
+
+    if not validate(args.srcFile):
+        print(f"{args.srcFile} doesn't validate")
+        sys.exit(1)
+
+    config = configparser.ConfigParser()
+    if args.config:
+        config.read_dict(DEFAULT_INI)
+        config.read(args.config)
+    else:
+        config.read_dict(DEFAULT_INI)
+
+    e = ipxactParser(args.srcFile, config)
+    document = e.returnDocument()
+    generator = ipxact2otherGenerator(args.destDir, config)
+    generator.generate(vhdlAddressBlock, document)
+

pyproject.toml
@@ -26,8 +26,20 @@ classifiers = [
 ]
 
 [build-system]
-requires = ["setuptools"]
+requires = ["setuptools", "wheel"]
 build-backend = "setuptools.build_meta"
+
+[tool.setuptools.packages.find]
+where = ["."]
+include = ["ipxact2systemverilog"]
+
+[project.scripts]
+ipxact2c = "ipxact2systemverilog.cli:main_c"
+ipxact2md = "ipxact2systemverilog.cli:main_md"
+ipxact2rst = "ipxact2systemverilog.cli:main_rst"
+ipxact2systemverilog = "ipxact2systemverilog.cli:main_systemverilog"
+ipxact2vhdl = "ipxact2systemverilog.cli:main_vhdl"
+
 
 [project.urls]
 Repository = "https://github.com/oddball/ipxact2systemverilog"